2c5548db35a8c5bec7977606cac62d792614e02f
Aayush Sharma <65645066+aayushsharma7@users.noreply.github.com>
2026-01-24 12:36:02 +0530

    Add files via upload

---
+++ src/models/videos.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const videoModel = new mongoose.Schema({
+    playlist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true        
+    },
+    channelId:{
+        type: String,
+        required: true        
+    },
+    channelTitle:{
+        type: String,
+        required: true        
+    },
+    thumbnail:{
+        type: String,
+    },
+    videoId:{
+        type: String,
+        required: true  
+    },
+    duration:{
+        type: String,
+        required:true
+    },
+    progressTime: {
+        type: Number,
+        default: 0
+    },
+    totalDuration: {
+        type: Number,
+        default: 0
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    notes:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Notes",
+    }]
+
+},{timestamps: true});
+
+
+export const Video = mongoose.model("Video", videoModel);
---
+++ src/routes/courseRoute.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { courseController, getAi,getVideoNotes,getSummary,getRecommendedProblems, getCourse,updateVideoNotes,deleteVideoNotes, getCourseData, getSingleCourse, getVideo, updateCourseProgess, updateVideoProgess } from "../controllers/course.controller.js";
+import { authCheck } from "../middlewares/authCheck.js";
+
+const router = express.Router()
+
+router.post('/create', authCheck, courseController);
+
+router.get('/', authCheck, getCourse);
+router.get('/getCourse/:id', authCheck, getSingleCourse);
+
+router.get('/data/:id', getCourseData)
+router.post('/update/course',authCheck, updateCourseProgess)
+router.post('/update/video',authCheck, updateVideoProgess)
+router.post('/update/video/notes',authCheck, updateVideoNotes)
+router.post('/update/video/notes/delete',authCheck, deleteVideoNotes)
+router.get('/notes/:id',authCheck, getVideoNotes);
+router.post('/generate/problems', authCheck, getRecommendedProblems);
+router.post('/generate/summary', authCheck, getSummary);
+
+
+router.get('/video/:id', getVideo)
+router.post('/ai',authCheck, getAi)
+
+export default router;
